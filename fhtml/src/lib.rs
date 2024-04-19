a0440a8d2d9e479108ec89a6b19b721b83b237ea
Jonah Lund <jonahlund@proton.me>
2024-04-20 00:17:56 +0200

    Add simple test cases

--- fhtml/src/lib.rs
+++ fhtml/src/lib.rs
@@ -16,5 +16,50 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {}
+    fn test_basic_html() {
+        assert_eq!(self::format!(<div></div>), "<div></div>");
+        assert_eq!(
+            self::format!(<p>"Hello, World!"</p>),
+            "<p>Hello, World!</p>"
+        );
+    }
+
+    #[test]
+    fn test_nested_html() {
+        assert_eq!(
+            self::format!(<div><span>"Nested"</span>" content"</div>),
+            "<div><span>Nested</span> content</div>"
+        );
+    }
+
+    #[test]
+    fn test_html_with_attributes() {
+        assert_eq!(
+            self::format!(<a href="https://example.com">"Link"</a>),
+            r#"<a href="https://example.com">Link</a>"#
+        );
+        assert_eq!(
+            self::format!(<span aria-hidden="true">"Hidden"</span>),
+            r#"<span aria-hidden="true">Hidden</span>"#
+        );
+    }
+
+    #[test]
+    fn test_dynamic_expressions() {
+        let user = "Alice";
+        assert_eq!(self::format!(<div>{user}</div>), "<div>Alice</div>");
+        assert_eq!(self::format!(<div>{1 + 1}</div>), "<div>2</div>");
+        assert_eq!(
+            self::format!(<div class={std::format!("foo {}", "bar")}></div>),
+            r#"<div class="foo bar"></div>"#
+        );
+    }
+
+    #[test]
+    fn test_special_characters() {
+        assert_eq!(
+            self::format!(<div>"&lt;Encoded&gt;"</div>),
+            "<div>&lt;Encoded&gt;</div>"
+        );
+    }
 }
