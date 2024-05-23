ade1551c8ae3f4cc1620259385fca43063a53aad
Jonah Lund <jonahlund@proton.me>
2024-05-23 15:12:52 +0200

    fix: change to new `const_format` macro

--- fhtml/tests/const_format.rs
+++ fhtml/tests/const_format.rs
@@ -2,6 +2,6 @@
 
 #[test]
 fn valid_expressions() {
-    assert_eq!(fhtml::formatcp!(<div>{10_u8}</div>), "<div>10</div>");
-    assert_eq!(fhtml::formatcp!(<div>{"foo"}</div>), "<div>foo</div>");
+    assert_eq!(fhtml::const_format!(<div>{10_u8}</div>), "<div>10</div>");
+    assert_eq!(fhtml::const_format!(<div>{"foo"}</div>), "<div>foo</div>");
 }
